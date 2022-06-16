80c2393727ab4dbac4495910ced8f19a4cf4e621
Pritam2850 <pritambehera113@gmail.com>
2022-06-16 13:12:13 +0530

    updated

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+ Hi guys...
+ This file contails deployment information.
+
+Thish is the line which is added in development branch......
+
+ This the new line which is added here.
+ The updated line is added here..........Have a look!
